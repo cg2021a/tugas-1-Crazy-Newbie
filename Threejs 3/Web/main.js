b936139cb5e15714e0b60e03c16804fd1d6ad6b1
Crazy-Newbie <ezie2001@gmail.com>
2021-12-18 00:26:18 +0800

    ...

--- Threejs 3/Web/main.js
+++ Threejs 3/Web/main.js
@@ -1,145 +1,150 @@
 import * as THREE from 'https://threejsfundamentals.org/threejs/resources/threejs/r132/build/three.module.js';
-import {OrbitControls} from 'https://threejsfundamentals.org/threejs/resources/threejs/r132/examples/jsm/controls/OrbitControls.js';
-import {OBJLoader} from 'https://threejsfundamentals.org/threejs/resources/threejs/r132/examples/jsm/loaders/OBJLoader.js';
-import {MTLLoader} from 'https://threejsfundamentals.org/threejs/resources/threejs/r132/examples/jsm/loaders/MTLLoader.js';
-
+import { OrbitControls } from 'https://threejsfundamentals.org/threejs/resources/threejs/r132/examples/jsm/controls/OrbitControls.js';
+import {GLTFLoader} from 'https://threejsfundamentals.org/threejs/resources/threejs/r132/examples/jsm/loaders/GLTFLoader.js';
 
 
 function main() {
   const canvas = document.querySelector('#c');
   const renderer = new THREE.WebGLRenderer({canvas});
+  renderer.shadowMap.enabled = true;
 
-  const fov = 45;
+  const fov = 75;
   const aspect = 2;  // the canvas default
   const near = 0.1;
   const far = 100;
   const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
-  camera.position.set(0, 10, 20);
+  camera.position.z = 3;
 
-  const controls = new OrbitControls(camera, canvas);
-  controls.target.set(0, 5, 0);
-  controls.update();
+    const controls = new OrbitControls(camera, canvas);
+    c
+  controls.target.set(0, 0, 0);
+  controls.update();    
 
-  const scene = new THREE.Scene();
-  scene.background = new THREE.Color('black');
 
-  renderer.shadowMap.enabled = true;
-  renderer.shadowMap.type = true;
-
+    
+    const scene = new THREE.Scene();
   
+    
+  var texture, material, plane;
 
-  {
-    const loader = new THREE.CubeTextureLoader();
-    const texture = loader.load([
-      'https://threejsfundamentals.org/threejs/resources/images/cubemaps/computer-history-museum/pos-x.jpg',
-      'https://threejsfundamentals.org/threejs/resources/images/cubemaps/computer-history-museum/neg-x.jpg',
-      'https://threejsfundamentals.org/threejs/resources/images/cubemaps/computer-history-museum/pos-y.jpg',
-      'https://threejsfundamentals.org/threejs/resources/images/cubemaps/computer-history-museum/neg-y.jpg',
-      'https://threejsfundamentals.org/threejs/resources/images/cubemaps/computer-history-museum/pos-z.jpg',
-      'https://threejsfundamentals.org/threejs/resources/images/cubemaps/computer-history-museum/neg-z.jpg',
-    ]);
-    scene.background = texture;
-  }
-  
-  {
-    const planeSize = 4000;
+  texture = THREE.ImageUtils.loadTexture( "crate1_diffuse.png" );
 
-    const loader = new THREE.TextureLoader();
-    const texture = loader.load('materials/crate1_diffuse.png');
-    const bumpTexture = loader.load('materials/crate1_bump.png');
-    texture.wrapS = THREE.RepeatWrapping;
-    texture.wrapT = THREE.RepeatWrapping;
-    texture.magFilter = THREE.NearestFilter;
-    const repeats = planeSize / 200;
-    texture.repeat.set(repeats, repeats);
+  // assuming you want the texture to repeat in both directions:
+  texture.wrapS = THREE.RepeatWrapping; 
+  texture.wrapT = THREE.RepeatWrapping;
 
-    const planeGeo = new THREE.PlaneGeometry(planeSize, planeSize);
-    const planeMat = new THREE.MeshPhongMaterial({
-      map: texture,
-      bumpTexture:bumpTexture,
-      side: THREE.DoubleSide,
-    });
-    const mesh = new THREE.Mesh(planeGeo, planeMat);
-    mesh.rotation.x = Math.PI * -.5;
-    scene.add(mesh);
-  }
+  // how many times to repeat in each direction; the default is (1,1),
+  //   which is probably why your example wasn't working
+  texture.repeat.set( 4, 4 ); 
 
-  {
-    const skyColor = 0xB1E1FF;  // light blue
-    const groundColor = 0xB97A20;  // brownish orange
-    const intensity = 2;
-    const light = new THREE.HemisphereLight(skyColor, groundColor, intensity);
-    scene.add(light);
-  }
+  material = new THREE.MeshLambertMaterial({ map : texture });
+  plane = new THREE.Mesh(new THREE.PlaneGeometry(60, 60), material);
+  plane.material.side = THREE.DoubleSide;
+  plane.position.y = -3;
+  plane.receiveShadow = true
+  plane.rotation.x = Math.PI / 2;
+
+  scene.add(plane);
 
   {
     const color = 0xFFFFFF;
     const intensity = 1;
     const light = new THREE.DirectionalLight(color, intensity);
-    light.position.set(0, 10, 5);
-    light.target.position.set(-5, 0, 0);
+    light.position.set(-1, 2, 4);
     scene.add(light);
-    scene.add(light.target);
   }
 
-  function frameArea(sizeToFitOnScreen, boxSize, boxCenter, camera) {
-    const halfSizeToFitOnScreen = sizeToFitOnScreen * 0.5;
-    const halfFovY = THREE.MathUtils.degToRad(camera.fov * .5);
-    const distance = halfSizeToFitOnScreen / Math.tan(halfFovY);
-    // compute a unit vector that points in the direction the camera is now
-    // in the xz plane from the center of the box
-    const direction = (new THREE.Vector3())
-        .subVectors(camera.position, boxCenter)
-        .multiply(new THREE.Vector3(1, 0, 1))
-        .normalize();
+  // scene.add(new THREE.PointLightHelper(pointlight, 0.1, 0xff0000));
+  scene.fog = new THREE.Fog(0xFFC0CB, 3, 20);
 
-    // move the camera to a position distance units way from the center
-    // in whatever direction the camera was from the center already
-    camera.position.copy(direction.multiplyScalar(distance).add(boxCenter));
+  const spotLight = new THREE.SpotLight( 0xffffff, 1.5 );
+  spotLight.position.set(10,20,-1.5);;
 
-    // pick some near and far values for the frustum that
-    // will contain the box.
-    camera.near = boxSize / 100;
-    camera.far = boxSize * 100;
+  spotLight.castShadow = true;
 
-    camera.updateProjectionMatrix();
+  spotLight.shadow.mapSize.width = 2048;
+  spotLight.shadow.mapSize.height = 2048;
 
-    // point the camera to look at the center of the box
-    camera.lookAt(boxCenter.x, boxCenter.y, boxCenter.z);
+  spotLight.shadow.camera.near = 1;
+  spotLight.shadow.camera.far = 100;
+  spotLight.shadow.camera.fov = 30;
+
+  scene.add( spotLight );
+  var cubeRenderTarget1 = new THREE.WebGLCubeRenderTarget(256, {
+    format: THREE.RGBFormat,
+    generateMipmaps: true,
+    minFilter: THREE.LinearMipmapLinearFilter,
+    encoding: THREE.sRGBEncoding,
+  });
+
+  var cubeCamera1 = new THREE.CubeCamera(1, 1000, cubeRenderTarget1);
+
+  var cubeRenderTarget2 = new THREE.WebGLCubeRenderTarget(256, {
+    format: THREE.RGBFormat,
+    generateMipmaps: true,
+    minFilter: THREE.LinearMipmapLinearFilter,
+    encoding: THREE.sRGBEncoding,
+  });
+
+  var cubeCamera2 = new THREE.CubeCamera(1, 1000, cubeRenderTarget2);
+
+  const refGeometry = new THREE.SphereGeometry(1, 32, 32);
+  const refMaterial = new THREE.MeshBasicMaterial({
+    envMap: cubeRenderTarget2.texture,
+    combine: THREE.MultiplyOperation,
+    reflectivity: 1,
+  });
+  const reflective = new THREE.Mesh(refGeometry, refMaterial);
+
+  reflective.castShadow = true;
+  reflective.receiveShadow = true;
+
+  reflective.position.set(3,0,0);
+  scene.add(reflective);
+    
+  const boxWidth = 1;
+  const boxHeight = 1;
+  const boxDepth = 1;
+  const geometry = new THREE.BoxGeometry(boxWidth, boxHeight, boxDepth);
+
+  
+    const gltfLoader = new GLTFLoader();
+    gltfLoader.load('gltf/scene.gltf', (gltf) => {
+        const root = gltf.scene;
+        root.position.set(-2, 0, 0);
+        root.traverse((o) => {
+            o.castShadow = true
+            o.receiveShadow = true
+          });
+      scene.add(root);
+    });
+    
+  function makeInstance(geometry, color, x) {
+    const material = new THREE.MeshPhongMaterial({color});
+
+      const cube = new THREE.Mesh(geometry, material);
+      cube.castShadow = true;
+    scene.add(cube);
+
+    cube.position.x = x;
+
+    return cube;
   }
 
-  var textureLoader = new THREE.TextureLoader();
-	
+  const cubes = [
+    makeInstance(geometry, 0x44aa88,  0),
+  ];
 
   {
-    const mtlLoader = new MTLLoader();
-    mtlLoader.load('https://threejsfundamentals.org/threejs/resources/models/windmill_2/windmill-fixed.mtl', (mtl) => {
-      mtl.preload();
-      const objLoader = new OBJLoader();
-      objLoader.setMaterials(mtl);
-      objLoader.load('https://threejsfundamentals.org/threejs/resources/models/windmill_2/windmill.obj', (root) => {
-        scene.add(root);
-
-        // compute the box that contains all the stuff
-        // from root and below
-        const box = new THREE.Box3().setFromObject(root);
-
-        const boxSize = box.getSize(new THREE.Vector3()).length();
-        const boxCenter = box.getCenter(new THREE.Vector3());      
-
-        // set the camera to frame the box
-        frameArea(boxSize * 2, boxSize, boxCenter, camera);
-
-        // update the Trackball controls to handle the new size
-        controls.maxDistance = boxSize * 10;
-        controls.target.copy(boxCenter);
-        controls.update();
+    const loader = new THREE.TextureLoader();
+    const texture = loader.load(
+      'beach_parking.jpg',
+      () => {
+        const rt = new THREE.WebGLCubeRenderTarget(texture.image.height);
+        rt.fromEquirectangularTexture(renderer, texture);
+        scene.background = rt.texture;
       });
-    });
   }
-  
-  
- 
 
   function resizeRendererToDisplaySize(renderer) {
     const canvas = renderer.domElement;
@@ -151,8 +156,10 @@ function main() {
     }
     return needResize;
   }
+  var count = 0;
 
-  function render() {
+  function render(time) {
+    time *= 0.001;
 
     if (resizeRendererToDisplaySize(renderer)) {
       const canvas = renderer.domElement;
@@ -160,6 +167,22 @@ function main() {
       camera.updateProjectionMatrix();
     }
 
+    cubes.forEach((cube, ndx) => {
+      const speed = 1 + ndx * .1;
+      const rot = time * speed;
+      cube.rotation.x = rot;
+      cube.rotation.y = rot;
+    });
+
+    reflective.visible = false
+    if (count % 2 === 0) {
+      cubeCamera1.update(renderer, scene);
+    } else {
+      cubeCamera2.update(renderer, scene);
+    }
+
+    count++;
+    reflective.visible = true
     renderer.render(scene, camera);
 
     requestAnimationFrame(render);
